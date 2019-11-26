251a74b41ca3324f300e213c94b723a4544a762d
SvetlanaKim3085 <57778306+SvetlanaKim3085@users.noreply.github.com>
2019-11-26 16:31:44 +1100

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,10 +1,10 @@
-import time
+from selenium.common.exceptions import NoSuchElementException
 
-link ="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_should_see_button(browser):
-    browser.get(link)
-    browser.execute_script("window.scrollBy(0, 100);")
-    button=browser.find_element_by_css_selector("#add_to_basket_form > button")
-    button_name=button.text
-    assert "Añadir al carrito"==button_name, f"Button_name did not match"
+def test_add_to_cart_button_is_presence(browser):
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+    try:
+        browser.find_element_by_css_selector('.btn.btn-lg.btn-primary.btn-add-to-basket')
+    except NoSuchElementException:
+        raise AssertionError('Did not matched')
+    import time; time.sleep(10)
